4f7ed3b1366ac1816225c69f94a55eadb22c1321
Samuel Mortenson <samuel@mortenson.coffee>
2017-10-16 06:31:53 -0700

    Improved docs.js

--- docs.js
+++ docs.js
@@ -2,39 +2,30 @@ const ghpages = require('gh-pages');
 const rebase = require('rebase');
 const fs = require('fs-extra');
 
-fs.removeSync('ame-components');
-fs.copySync('www', 'ame-components');
+let subdir = 'ame-components';
+
+fs.removeSync(subdir);
+fs.copySync('www', subdir);
 
 let files = ['index.html', 'handlers.html'];
 files.forEach((filename) => {
-  let path = 'ame-components/' + filename;
+  let path = subdir + '/' + filename;
   let file = fs.readFileSync(path, 'utf8');
+  let replacements = {
+    '^\/(?!\/)': '/' + subdir + '/',
+    '^(?!\/|http|https)': '/' + subdir + '/',
+  };
   let rebased = rebase(file, {
-    url: {
-      '^(?=[^/])': '/ame-components/',
-      '^\/$': '/ame-components/',
-    },
-    a: {
-      '^(?=[^/])': '/ame-components/',
-      '^\/$': '/ame-components/',
-    },
-    img: {
-      '^(?=[^/])': '/ame-components/',
-      '^\/$': '/ame-components/',
-    },
-    link: {
-      '^(?=[^/])': '/ame-components/',
-      '^\/$': '/ame-components/',
-    },
-    script: {
-      '^(?=[^/])': '/ame-components/',
-      '^\/$': '/ame-components/',
-    }
+    url: replacements,
+    a: replacements,
+    img: replacements,
+    link: replacements,
+    script: replacements
   });
   fs.writeFileSync(path, rebased);
 });
 
-ghpages.publish('ame-components', function (err) {
+ghpages.publish(subdir, function (err) {
   if (err) {
     console.log(err);
   }
